022885cb5873bfa7c33c0f369b2c3a9ef17d547d
Christina71 <mariachristina71@yahoo.com>
2018-01-26 10:13:02 -0500

    bonus commit

--- src/main/java/com/example/demo/FizzBuzz.java
+++ src/main/java/com/example/demo/FizzBuzz.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Scanner;
+
 @RestController
 public class FizzBuzz {
 
@@ -29,4 +31,26 @@ public class FizzBuzz {
 
              return output;
     }
+
+    @RequestMapping ("/bonus")
+    public String bonus() {
+        Scanner input = new Scanner(System.in);
+        String output2 = "";
+        int count7 = 0, count17 = 0, count23 = 0;
+        System.out.println("The last number is");
+        int lastNum = input.nextInt();
+        for (int i = 1; i <= lastNum; i++) {
+            if (i % 7 == 0)
+                count7++;
+            if (i % 17 == 0)
+                count17++;
+            if (i % 23 == 0)
+                count23++;
+            output2 =  fizzBuzz()+"the number of multiples of 7 are " + String.valueOf(count7) + "</br>" + "the number of multiples of 17 are" + String.valueOf(count17) + "</br>" + "the number of multiples of 23 are" + String.valueOf(count23) + "</br>";
+
+
+        }
+        return output2;
+    }
 }
+
